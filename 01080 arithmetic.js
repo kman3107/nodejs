fffed9d095c5d1ffec3cb589280108f7819833cc
Kman3107 <kim.r.simon@live.com>
2023-02-12 22:47:54 +0100

    created arithmetic.js

---
+++ 01080 arithmetic.js
@@ -0,0 +1,24 @@
+
+// Binary operators - two values operators
+// Addition
+console.log(5 + 4);
+// Subtraction
+console.log(9 - 4);
+// Multiplication
+console.log(3 * 4);
+// Division
+console.log(7 / 2);
+// Mod operator - gives the remainder of division
+console.log(7 % 2);
+console.log(12.3 % 5);
+
+// Uniary operators - one value operators
+let value = 2;
+value++;
+console.log(value);
+++value;
+console.log(value);
+value--;
+console.log(value);
+--value;
+console.log(value);

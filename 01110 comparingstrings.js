7be674503209ed26bd56c433d354f04829ab406b
Kman3107 <kim.r.simon@live.com>
2023-02-13 19:12:57 +0100

    created comparingstrings.js

---
+++ 01110 comparingstrings.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let text = "Cat";
+console.log("Text == 'Cat'", text == "Cat");
+console.log("Text === 'Cat'", text === "Cat");
+
+console.log("Text != 'Cat'", text != "Cat");
+console.log("Text !== 'Cat'", text !== "Cat");
+
+console.log("Text < 'Dog'", text < "Dog");
+console.log("Text > 'Dog'", text > "Dog");
+
+console.log("Text <= 'Dog'", text <= "Dog");
+console.log("Text >= 'Dog'", text >= "Dog");

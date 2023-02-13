e98a12c9674dc8449750375dee5ffb33d3386088
Kman3107 <kim.r.simon@live.com>
2023-02-13 19:30:06 +0100

    created identity.js

---
+++ 01120 identity.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let days = 7;
+
+// True
+console.log("days == '7'", days == "7");
+// False
+console.log("days === '7'", days === "7");
+// False
+console.log("days != '7'", days != "7");
+// True
+console.log("days !== '7'", days !== "7");

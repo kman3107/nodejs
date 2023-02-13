401eeb411cad6c48b52abdae30d5bde870d7922e
Kman3107 <kim.r.simon@live.com>
2023-02-13 16:18:22 +0100

    created comparingnumbers.js

---
+++ 01100 comparingnumbers.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Is equal - True
+console.log("7 equal 7:", 7 == 7);
+
+// Is not equal - True
+console.log("7 is not equal 4:", 7 != 4);
+
+// Is identical - True
+console.log("7 identical 7:", 7 === 7);
+
+// Is not identical - True
+console.log("7 is not identical 4:", 7 !== 4);
+
+// Is greater - True
+console.log("12 is greater than 10:", 12 > 10);
+
+// Is less - True
+console.log("1 is less than 3:", 1 < 3);
+
+// Is greater than or equal - False
+console.log("7 is greater than or equal to 10:", 7 >= 10);
+
+// Is less than or equal - False
+console.log("7 is less than or equal to 2:", 7 <= 2);

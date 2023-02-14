0ebb06b5004f26b6a12c34bda37da3b01303dcec
Kman3107 <kim.r.simon@live.com>
2023-02-14 14:35:11 +0100

    created for.js

---
+++ 01160 for.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// ';;' makes an infinite loop
+// [1] run first loop, [2] condition check before every loop, [3] run after every loop
+for(let i = 0; i < 5; i++) {
+    console.log("Loop:", i)
+}
